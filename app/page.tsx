d4b7aea319f873ac46d7bdfb9abefc7e4d10b111
Christian Keogh <christianfkeogh@gmail.com>
2024-12-01 11:52:44 +0000

    adding punctuation test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <section>
       <p className="mb-4">
-        {`I'm a Frontend Angular Developer from Dublin. I use this website to blog about things that interest me but mostly what annoys me`}
+        {`I'm a Frontend Angular Developer from Dublin. I use this website to blog about things that interest me but mostly what annoys me.`}
       </p>
     </section>
   );
