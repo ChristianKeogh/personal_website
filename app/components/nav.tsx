eba3670ffdbcc5f21fcfbb4d7e72e9c2030cd0ec
Christian Keogh <christianfkeogh@gmail.com>
2024-12-15 21:29:20 +0000

    add photo of myself and centered navbar

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -14,13 +14,13 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <aside className="mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex justify-center items-center relative px-0 pb-0 fade md:overflow-auto scroll-pr-6"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          <div className="flex flex-wrap justify-center space-x-6">
             {Object.entries(navItems).map(([path, { name }]) => {
               const isChristianKeogh = name === "christian keogh";
               return (
@@ -52,7 +52,7 @@ export function Navbar() {
               target="_blank"
               href="https://www.linkedin.com/in/christian-keogh-94888a28b/"
             >
-              <p className="  h-7">linkedin</p>
+              <p className="h-7">linkedin</p>
             </a>
           </div>
         </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
-    <section>
-      <p className="mb-4">
+    <section className="flex flex-col items-center">
+      <img src="/images/home/IMG_4172.jpg" alt="Mineself" className="mb-4" />
+      <br />
+      <p className="mb-4 text-justify">
         {`I'm a Frontend Angular Developer from Dublin. I use this website to blog about things.`}
       </p>
     </section>
