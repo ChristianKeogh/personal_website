f67cc060cf96146850822668a80bde0a7e4d1a87
Christian Keogh <christianfkeogh@gmail.com>
2025-01-11 10:10:02 +0000

    Added WIP message on main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,12 @@ export default function Page() {
       <img src="/images/home/IMG_4172.jpg" alt="Mineself" className="mb-4" />
       <br />
       <p className="mb-4 text-justify">
-        {`I'm a Frontend Developer from Dublin. I use this website to blog about things.`}
+        I'm a Frontend Developer from Dublin. I use this website to blog about
+        things.
+      </p>
+      <br /> <br /> <br />
+      <p className="text-neutral-400">
+        This website will be in a perpetual state of WIP for the time being.
       </p>
     </section>
   );
